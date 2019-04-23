259ff355d5e654c59a16a40d73dd6522e51832f7
Svyatoslav <svyatoslav912@gmail.com>
2019-04-23 15:14:01 +0300

    Jenkins install

---
+++ bash-scripts/centos/install/jenkins-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+yum install -y java-1.8.0-openjdk-devel
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install -y jenkins
+systemctl start jenkins
+systemctl enable jenkins
+firewall-cmd --zone=public --add-port=8080/tcp
+firewall-cmd --runtime-to-permanent
